cb547c8b49d91b1119ac1e57b8bec2bf92963987
Léon Barriault <165422257+Leon-Barriault@users.noreply.github.com>
2026-08-10 13:14:18 -0400

    feat(manager): enable native planet printing

--- android/manager/build.gradle.kts
+++ android/manager/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.print:print:1.1.0")
 
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
